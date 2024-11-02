cc1d97e1d6cd41853d708dc20df9cb1f83ced00c
NaaluZeroNaalu <ajithruflex1210@gmail.com>
2024-11-02 13:23:01 +0530

    update contact and whatsapp number

--- src/pages/sidecontact/side.jsx
+++ src/pages/sidecontact/side.jsx
@@ -9,12 +9,12 @@ function Sidecontent(){
         <>
          {/* Contact Buttons Section */}
       <div className="fixed top-56 right-4 flex flex-col gap-2 z-50">
-        <button className="bg-blue-500 text-white flex items-center px-4 py-3  mb-2" style={{borderRadius:"5px"}}>
+        <a href="tel:+9500262708" target="_blank" className="bg-blue-500 text-white no-underline flex items-center px-4 py-3  mb-2" style={{borderRadius:"5px"}}>
           <FaPhoneAlt className="mr-2" /> Call Us
-        </button>
-        <button className="bg-green-500 text-white flex items-center px-4 py-3 " style={{borderRadius:"5px"}}>
+        </a>
+        <a href="https://wa.me/9500262708" target="_blank" className="bg-green-500 text-white no-underline flex items-center px-4 py-3 " style={{borderRadius:"5px"}}>
           <FaWhatsapp className="mr-2" /> WhatsApp
-        </button>
+        </a>
       </div>
         </>
     )
