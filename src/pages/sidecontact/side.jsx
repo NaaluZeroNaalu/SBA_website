03e521a6faa8258ebe5465bc7ab856f11f117f5f
NaaluZeroNaalu <ajithruflex1210@gmail.com>
2025-01-22 16:52:42 +0530

    added chat bot

--- src/pages/sidecontact/side.jsx
+++ src/pages/sidecontact/side.jsx
@@ -8,14 +8,15 @@ function Sidecontent(){
 
         <>
          {/* Contact Buttons Section */}
-      <div className="fixed mt-4 right-4 flex flex-col gap-2 z-50">
-        <a href="tel:+91 9500262708" target="_blank" className="bg-blue-500 text-white no-underline flex items-center px-4 py-3 " style={{borderRadius:"5px"}}>
-          <FaPhoneAlt className="mr-2" />
+      <div className="fixed mt-4 right-0 flex flex-col  z-50">
+        <a href="tel:+91 9500262708" target="_blank" className="bg-blue-500 text-white no-underline flex items-center px-4 py-3 ">
+          <FaPhoneAlt className="" />
         </a>
-        <a href="https://wa.me/919500262708" target="_blank" className="bg-green-500 text-white no-underline flex items-center px-4 py-3 " style={{borderRadius:"5px"}}>
-          <FaWhatsapp className="mr-2" />
+        <a href="https://wa.me/919500262708" target="_blank" className="bg-green-500 text-white no-underline flex items-center px-4 py-3 ">
+          <FaWhatsapp className="" />
         </a>
       </div>
+      
         </>
     )
 }
