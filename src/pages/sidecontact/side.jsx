49baa652f4cb17b35148dfa9b26dab1a479c84ed
NaaluZeroNaalu <ajithruflex1210@gmail.com>
2025-01-31 14:38:24 +0530

    returned old changes in side panel

--- src/pages/sidecontact/side.jsx
+++ src/pages/sidecontact/side.jsx
@@ -11,12 +11,12 @@ function Sidecontent(){
          
          fixed mt-4 right-0 flex flex-col  z-20
          */}
-      <div className="fixed top-40 flex flex-col right-0 mt-4 z-30" style={{}}>
-        <a href="tel:+91 9500262708" target="_blank" className=" text-white no-underline flex items-center px-4 py-3 ">
-          <FaPhoneAlt className="" style={{color:"blue"}} />
+      <div className="fixed top-30 flex flex-col right-0 mt-4 z-30" style={{}}>
+        <a href="tel:+91 9500262708" target="_blank" className="bg-blue-500 text-white no-underline flex items-center px-4 py-3 ">
+          <FaPhoneAlt className="" />
         </a>
-        <a href="https://wa.me/919500262708" target="_blank" className=" text-white no-underline flex items-center px-4 py-3 ">
-          <FaWhatsapp className="" style={{color:"green"}} />
+        <a href="https://wa.me/919500262708"  target="_blank" className="bg-green-500 text-white no-underline flex items-center px-4 py-3 ">
+          <FaWhatsapp className="" />
         </a>
       </div>
         </>
